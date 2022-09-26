294c0f714fdd4ed4ae3c2118cf2b413263fba249
Alonso Paucar <iaquintanapa@gmail.com>
2022-09-25 22:37:07 -0500

    Insertando lo esencial

---
+++ src/app/shared/siderbar/siderbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-siderbar',
+  templateUrl: './siderbar.component.html',
+})
+export class SiderbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
